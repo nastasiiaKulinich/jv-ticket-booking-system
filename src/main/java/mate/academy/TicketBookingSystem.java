dd32f790b72e30aca83b2e3500bf962f50bbf42c
nastasiiaKulinich <kussik2004@gmail.com>
2025-07-02 09:17:50 +0300

    added semaphore to the TicketBookingSystem

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,18 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        semaphore = new Semaphore(totalSeats, true);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (semaphore.tryAcquire()) {
+            return new BookingResult(user, true, "Booking successful.");
+        }
+        return new BookingResult(user, false, "No seats available.");
     }
 }
